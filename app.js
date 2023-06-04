ebf30c2c0e98609b2306ca1c340dd2fc3763d5ba
RahulSelvakumar <112105461+RahulSelvakumar@users.noreply.github.com>
2023-06-04 15:00:50 +0530

    added bcrypt hashing function

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 const app=express();
 
 app.set("view engine","ejs");
@@ -28,36 +29,44 @@ app.get("/login",function(req,res){
 })
 app.post("/login",function(req,res){
     const username=req.body.username;
-    const password=md5(req.body.password);
+    const password=req.body.password;
     Users.findOne({email:username},function(err,foundUser){
-        if(err){
+        if(err)
+            {
             res.send(err);
-        }
-        else{
-            if(foundUser.password===password){
-                res.render("secrets")
             }
-        }
-    })
-})
+        else{
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if(result===true){
+                    res.render('secrets');
+                }
+                else{
+                    res.send(err);
+                    }
+            });
+            }            
+        });
+});
 
 app.get("/register",function(req,res){
     res.render("register");
 })
 app.post("/register",function(req,res){
-    const newUser=new Users({
-        email:req.body.username,
-        password:md5(req.body.password)
-    })
-    newUser.save(function(err){
-        if(err){
-            res.send(err);
-        }
-        else{
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser=new Users({
+            email:req.body.username,
+            password:hash
+        })
+        newUser.save(function(err){
+            if(err){
+                res.send(err);
+            }
+            else{
+                res.render("secrets");
+            }
+        });     
     });
-
+   
 })
 
 
