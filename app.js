6707a6ad71183ac65a9d81d8901e2f7536e49dde
RahulSelvakumar <112105461+RahulSelvakumar@users.noreply.github.com>
2023-06-04 09:49:15 +0530

    added hash function for password

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 const app=express();
 
 app.set("view engine","ejs");
@@ -17,8 +17,6 @@ const userSchema=new mongoose.Schema({
     password:String
 });
 
-userSchema.plugin(encrypt, { secret:process.env.SECRET,encryptedFields:["password"] } );
-
 const Users=new mongoose.model("user",userSchema); 
 
 app.get("/",function(req,res){
@@ -30,7 +28,7 @@ app.get("/login",function(req,res){
 })
 app.post("/login",function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
     Users.findOne({email:username},function(err,foundUser){
         if(err){
             res.send(err);
@@ -49,7 +47,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser=new Users({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     })
     newUser.save(function(err){
         if(err){
